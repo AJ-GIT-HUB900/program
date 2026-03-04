c06092a3ef81e848a12c0465f0114509daac1d20
Ajit Thapa <ajitthapa78@gmail.com>
2026-03-04 01:55:22 -0800

    Enhance matrix addition program structure and input validation
    Refactored matrix addition program for improved organization and error handling.

--- program/add-two-matrices/AddTwoMatrices.c
+++ program/add-two-matrices/AddTwoMatrices.c
@@ -1,43 +1,60 @@
-// C program for addition of two matrices of size n*n
+// Improved C program for addition of two n*n matrices
 #include <stdio.h>
 #define MAX 100
 
-int main()
-{
-    int i, j, n;
+// Function prototypes to keep our code organized
+void inputMatrix(int mat[MAX][MAX], int n, const char* name);
+void addMatrices(int mat1[MAX][MAX], int mat2[MAX][MAX], int result[MAX][MAX], int n);
+void printMatrix(int mat[MAX][MAX], int n);
+
+int main() {
+    int n;
     int mat1[MAX][MAX], mat2[MAX][MAX], mat3[MAX][MAX];
 
-    // Input size of n*n matrix
-    scanf("%d", &n);
-
-    // Input Elements for First Matrix
-    for(i=0; i<n; i++)
-    {
-        for(j=0; j<n; j++)
-            scanf("%d", &mat1[i][j]);
+    // Input size with basic error handling
+    printf("Enter the size of the n*n matrix (Max %d): ", MAX);
+    if (scanf("%d", &n) != 1 || n <= 0 || n > MAX) {
+        printf("Invalid input. Please enter a valid size between 1 and %d.\n", MAX);
+        return 1; // Exit the program with an error code
     }
 
-   // Input Elements for Second Matrix
-    for(i=0; i<n; i++)
-    {
-        for(j=0; j<n; j++)
-            scanf("%d", &mat2[i][j]);
-    }
+    // Call our functions to handle the heavy lifting
+    inputMatrix(mat1, n, "First");
+    inputMatrix(mat2, n, "Second");
 
-    // Addition
-    for(i=0; i<n; i++)
-    {
-        for(j=0; j<n; j++)
-            mat3[i][j] = mat1[i][j] + mat2[i][j];
-    }
+    addMatrices(mat1, mat2, mat3, n);
 
-    // Show addition result
-    for(i=0; i<n; i++)
-    {
-        for(j=0; j<n; j++)
-            printf("%d ", mat3[i][j]);
-        printf("\n");
-    }
+    printf("\nResult of Matrix Addition:\n");
+    printMatrix(mat3, n);
 
     return 0;
+}
+
+// Function to handle gathering user input
+void inputMatrix(int mat[MAX][MAX], int n, const char* name) {
+    printf("Enter elements for the %s Matrix:\n", name);
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &mat[i][j]);
+        }
+    }
+}
+
+// Function strictly dedicated to the math
+void addMatrices(int mat1[MAX][MAX], int mat2[MAX][MAX], int result[MAX][MAX], int n) {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            result[i][j] = mat1[i][j] + mat2[i][j];
+        }
+    }
+}
+
+// Function strictly dedicated to displaying the output
+void printMatrix(int mat[MAX][MAX], int n) {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
 }
